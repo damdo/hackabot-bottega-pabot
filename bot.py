78efb24c2b3daf5e401be590121b29efb9dff218
Damiano Donati <damiano.donati@gmail.com>
2018-03-03 13:51:55 +0100

    imporvements, rendering processes with browse src

--- bot.py
+++ bot.py
@@ -10,22 +10,24 @@ import json
 
 # Get token bot from the corresponding environment variable
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-ANALYTICS_ENDPOINT = "http://tgbot-dash:8888/api"
+NLP_BACKEND_ENDPOINT = "http://tgbot-nlp-backend:5000/"
 ANALYTICS_ENDPOINT = "http://tgbot-dash:8888/api"
 
+# Store cat response for cache
+cat_response = {}
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # define the states to fall into
-CHOOSING, TYPING_REPLY, TYPING_CHOICE, WANT_FEEDBACK, FEEDBACK, SPECIFIED_FEEDBACK = range(6)
+CHOOSING, TYPING_REPLY, TYPING_CHOICE, WANT_FEEDBACK, SPECIFIED_FEEDBACK, RECOVER, RENDERING_PROCESS = range(7)
 
 # define the reply buttoned keyboard
 # a row is a list of columns, rows are separated by a comma and contained in a list
 choice_buttons = [
         ['DOMANDA LIBERA'],
         ['SFOGLIA PER CATEGORIE'],
-        ['Done']
+        ['Fine']
 ]
 
 feedback_buttons = [
@@ -35,7 +37,7 @@ feedback_buttons = [
 
 want_feedback_buttons = [
     ["No"],
-    ["Yes"]
+    ["Si"]
 ]
 
 feedback_keyboard = ReplyKeyboardMarkup(feedback_buttons, one_time_keyboard=True)
@@ -67,17 +69,13 @@ def facts_to_str(user_data):
 def start(bot, update, user_data):
     last_command = "start"
     send_to_analytics(bot, update, user_data, last_command)
-    # pass the bot and the update
-    # update.message.reply_text
     update.message.reply_text("Ciao "+update.message.from_user.first_name+" sono PAbot, il bot della Provincia di Trento! Sono qui per aiutarti e guidarti nelle procedure della pubblica amministrazione, sono esperto in: \n - PROCEDIMENTI \n - MODULI DI DOMANDA \n - CERTIFICAZIONI \n \n Puoi chiedermi quello che ti serve in due modi: \n - Clicca DOMANDA LIBERA per cercare tra le procedure \n oppure \n - Clicca SFOGLIA PER CATEGORIE e cerca quello che ti serve", reply_markup=choice_keyboard)
-    # update.message.reply_text( """Ciao sono PAbot, il bot della Provincia di Trento! Come posso esserti utile?""", reply_markup=markup_things)
     return CHOOSING
 
 
 def regular_choice(bot, update, user_data):
     text = update.message.text
-    user_data['choice'] = text
-    update.message.reply_text( 'Bene :) Hai scelto DOMANDA LIBERA, scrivimi pure qui sotto quello che stai cercando, faro\' del mio meglio per aiutarti!'.format(text.lower()))
+    update.message.reply_text( 'Bene :) Hai scelto DOMANDA LIBERA, scrivimi pure qui sotto quello che stai cercando, faro\' del mio meglio per aiutarti!')
 
     return TYPING_REPLY
 
@@ -85,7 +83,7 @@ def want_feedback_process(bot, update, user_data):
     print "want_feedback_process"
     text = update.message.text
     print text
-    if text == "Yes" :
+    if text == "Si" :
         print "setting FEEDBACK"
         update.message.reply_text("Indica un punteggio per il bot da 1 a 5 star :)", reply_markup=feedback_keyboard)
         return SPECIFIED_FEEDBACK
@@ -93,15 +91,38 @@ def want_feedback_process(bot, update, user_data):
         update.message.reply_text('Grazie per aver utilizzato il bot, A presto!')
         return ConversationHandler.END
 
-        return ConversationHandler.END
-
 def custom_choice(bot, update):
-    update.message.reply_text('Bene :) Hai scelto SFOGLIA PER CATEGORIE, guarda i pulsanti delle categorie qui sotto e scegli quella piu\' pertinente alle tue necessita\'')
+    cat_buttons = []
+    r = requests.post(NLP_BACKEND_ENDPOINT+"cat")
+    headers = {'Content-type': 'application/json'}
+    cat_response = r.json()
+    for k,v in cat_response["processes"].iteritems():
+        cat_buttons.append([k])
+    cat_keyboard = ReplyKeyboardMarkup(cat_buttons, one_time_keyboard=True)
+    update.message.reply_text('Bene :) Hai scelto SFOGLIA PER CATEGORIE, guarda i pulsanti delle categorie qui sotto e scegli quella piu\' pertinente alle tue necessita\'', reply_markup=cat_keyboard)
     return TYPING_CHOICE
 
 
 def received_information(bot, update, user_data):
-    update.message.reply_text("TODO: (Questa sara\' la risposta alla domanda dell\'utente)", reply_markup=choice_keyboard)
+    user_free_text_request = update.message.text
+
+    r = requests.post(NLP_BACKEND_ENDPOINT+"req",json={"input_string": user_free_text_request})
+    headers = {'Content-type': 'application/json'}
+    response_procedures = r.json()
+    print type(response_procedures)
+    print response_procedures
+
+    if response_procedures["processes"]["ERROR"] == True:
+        update.message.reply_text("Mi dispiace, non sono riuscito a trovare una risposta alla tua domanda :( prova a chiedemi qualcosa di diverso o altrimenti prova a SFOGLIARE PER CATEGORIE", reply_markup=choice_keyboard)
+    else:
+        procedures_buttons = []
+        for key,value in response_procedures["processes"].iteritems():
+            if "ERROR" not in key:
+                procedures_buttons.append([value["title"]])
+        procedures_keyboard = ReplyKeyboardMarkup(procedures_buttons, one_time_keyboard=True)
+
+        update.message.reply_text("Bene, questi sono i risultati più pertinenti che ho trovato per la tua domanda:\n", reply_markup=procedures_keyboard)
+
     return CHOOSING
 
 
@@ -109,20 +130,53 @@ def done(bot, update, user_data):
     update.message.reply_text("Ottimo, spero tu abbia trovato la risposta che stavi cercando. Desideri lasciarci un feedback?", reply_markup=want_feedback_keyboard)
     return WANT_FEEDBACK
 
-#def feedback(bot,update,user_data):
-#    print "feedback fun"
-#    update.message.reply_text("Indica un punteggio per il bot da 1 a 5 star :)", reply_markup=feedback_keyboard)
-#    return SPECIFIED_FEEDBACK
 
 def exit_process(bot, update, user_data):
     update.message.reply_text('Grazie per aver utilizzato il bot, A presto!')
     return ConversationHandler.END
 
+def cat_handler(bot, update, user_data):
+    processes_buttons = []
+    text = update.message.text
+
+    user_data["current_cat"] = text;
+
+    r = requests.post(NLP_BACKEND_ENDPOINT+"cat")
+    headers = {'Content-type': 'application/json'}
+    cat_response = r.json()
+
+    processes_list_for_single_cat = cat_response["processes"][text]
+    for proc in processes_list_for_single_cat:
+        processes_buttons.append([proc["NOME"]])
+
+    print processes_buttons
+
+    processes_keyboard = ReplyKeyboardMarkup(processes_buttons, one_time_keyboard=True)
+    update.message.reply_text('Bene, questi sono i risultati più pertinenti che ho trovato per la tua domanda:\n', reply_markup=processes_keyboard)
+    return RENDERING_PROCESS
+
+
+def rendering_process_handler(bot, update, user_data):
+    input_proccess_name = update.message.text
+
+    r = requests.post(NLP_BACKEND_ENDPOINT+"cat")
+    headers = {'Content-type': 'application/json'}
+    cat_response = r.json()
+    extracted_process = {"NOME": "Mi dispiace", "DESCRIZIONE": "Processo non trovato, riprova."}
+    for process in cat_response["processes"][user_data["current_cat"]]:
+        if process["NOME"] == input_proccess_name:
+            extracted_process = process
+
+    update.message.reply_text(process["NOME"]+"\n "+process["DESCRIZIONE"]+"\n"+process["URL DEL SITO"]+"\n",reply_markup=choice_keyboard)
+    return CHOOSING
 
 def error(bot, update, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
 
+def not_understood(bot, update, user_data):
+    update.message.reply_text('Scusa, non ho capito cosa intendi, prova a chiedermi qualcosa oppure, clicca uno dei pulsanti qui sotto', reply_markup=choice_keyboard)
+    return RECOVER
 
 def main():
     # Create the Updater and pass it your bot's token.
@@ -135,14 +189,47 @@ def main():
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start, pass_user_data=True)],
         states={
-            CHOOSING: [RegexHandler('^(DOMANDA LIBERA|ALTRO)$', regular_choice, pass_user_data=True), RegexHandler('^SFOGLIA PER CATEGORIE$', custom_choice), ],
-            WANT_FEEDBACK: [RegexHandler('^(Yes|No)$', want_feedback_process, pass_user_data=True)],
-            SPECIFIED_FEEDBACK: [MessageHandler(Filters.text, exit_process , pass_user_data=True)],
-            #FEEDBACK: [RegexHandler('No',feedback, pass_user_data=True), RegexHandler('Yes',feedback, pass_user_data=True)],
-            TYPING_CHOICE: [MessageHandler(Filters.text, regular_choice, pass_user_data=True), ],
-            TYPING_REPLY: [MessageHandler(Filters.text, received_information, pass_user_data=True), ],
+
+            CHOOSING: [
+                RegexHandler('^(DOMANDA LIBERA)$', regular_choice, pass_user_data=True),
+                RegexHandler('^SFOGLIA PER CATEGORIE$', custom_choice),
+                RegexHandler('^Fine$', done, pass_user_data=True), MessageHandler(Filters.text, not_understood, pass_user_data=True),
+            ],
+
+            WANT_FEEDBACK: [
+                RegexHandler('^(Si|No)$', want_feedback_process, pass_user_data=True),
+                RegexHandler('^Fine$', done, pass_user_data=True), MessageHandler(Filters.text, not_understood, pass_user_data=True),
+            ],
+
+            SPECIFIED_FEEDBACK: [
+                RegexHandler('^Fine$', done, pass_user_data=True),
+                MessageHandler(Filters.text, exit_process , pass_user_data=True),
+            ],
+
+            TYPING_CHOICE: [
+                RegexHandler('^Fine$', done, pass_user_data=True),
+                MessageHandler(Filters.text, cat_handler, pass_user_data=True),
+            ],
+
+            RECOVER: [
+                RegexHandler('^(DOMANDA LIBERA)$', regular_choice, pass_user_data=True),
+                RegexHandler('^SFOGLIA PER CATEGORIE$', custom_choice),
+                RegexHandler('^Fine$', done, pass_user_data=True), MessageHandler(Filters.text, not_understood, pass_user_data=True),
+            ],
+
+            TYPING_REPLY: [
+                MessageHandler(Filters.text, received_information, pass_user_data=True),
+            ],
+
+            RENDERING_PROCESS: [
+                MessageHandler(Filters.text, rendering_process_handler, pass_user_data=True),
+            ],
         },
-        fallbacks=[RegexHandler('^Done$', done, pass_user_data=True)]
+
+        fallbacks=[
+            RegexHandler('^Fine$', done, pass_user_data=True),
+            MessageHandler(Filters.text, not_understood, pass_user_data=True),
+        ]
     )
 
     dp.add_handler(conv_handler)
